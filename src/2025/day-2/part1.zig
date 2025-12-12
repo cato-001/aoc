e7f7dc37d44674729ee6477942226b6e493eafdd
l.weber <l.weber@traveltrex.com>
2025-12-12 19:32:02 +0100

    feat: refactor zig day2

--- src/2025/day-2/part1.zig
+++ src/2025/day-2/part1.zig
@@ -27,42 +27,42 @@ pub fn main() !void {
 
         const end = parseInt(range[startNumberLength+1..]);
 
-        var number = start;
         var numberBuffer: [20]u8 = undefined;
+        var number = start;
         var numberStr = range[0..startNumberLength];
-        var length = numberStr.len;
-        var numberFirstHalf = parseInt(numberStr[0..length>>1]);
+        var numberFirstHalf = parseInt(numberStr[0..numberStr.len>>1]);
 
-        if (length&1 == 1) {
-            numberFirstHalf = std.math.pow(usize, 10, length>>1);
-            number = std.math.pow(usize, 10, length) + numberFirstHalf;
-            length += 1;
+        if (numberStr.len&1 == 1) {
+            numberFirstHalf = std.math.pow(usize, 10, numberStr.len>>1);
+            number = std.math.pow(usize, 10, numberStr.len) + numberFirstHalf;
+            numberStr.len += 1;
             numberBuffer = @splat('0');
-            numberStr = numberBuffer[0..length];
+            numberStr = numberBuffer[0..numberStr.len];
             numberStr[0] = '1';
         }
 
-        @memcpy(numberStr[length>>1..length], numberStr[0..length>>1]);
-        number = numberFirstHalf * std.math.pow(usize, 10, length>>1) + numberFirstHalf;
+        @memcpy(numberStr[numberStr.len>>1..numberStr.len], numberStr[0..numberStr.len>>1]);
+        number = numberFirstHalf * std.math.pow(usize, 10, numberStr.len>>1) + numberFirstHalf;
 
         while (number <= end) {
-            if (start <= number and std.mem.eql(u8, numberStr[0..length>>1], numberStr[length>>1..length])) {
+            if (start <= number and std.mem.eql(u8, numberStr[0..numberStr.len>>1], numberStr[numberStr.len>>1..numberStr.len])) {
                 total += number;
             }
 
             numberFirstHalf += 1;
-            const increased = increase1(numberStr[0..length>>1]);
-            if (increased == null) {
-                length += 2;
+            number = numberFirstHalf * std.math.pow(usize, 10, numberStr.len>>1) + numberFirstHalf;
+
+            const increased = increase1(numberStr[0..numberStr.len>>1]);
+            if (!increased) {
+                numberStr.len += 2;
                 numberBuffer = @splat('0');
-                numberStr = numberBuffer[0..length];
+                numberStr = numberBuffer[0..numberStr.len];
                 numberStr[0] = '1';
+                numberStr[numberStr.len>>1] = '1';
                 continue;
             }
 
-            @memcpy(numberStr[0..length>>1], increased.?);
-            @memcpy(numberStr[length>>1..length], increased.?);
-            number = numberFirstHalf * std.math.pow(usize, 10, length>>1) + numberFirstHalf;
+            @memcpy(numberStr[numberStr.len>>1..numberStr.len], numberStr[0..numberStr.len>>1]);
         }
     }
 
@@ -78,7 +78,7 @@ fn parseInt(text: []const u8) usize {
     return value;
 }
 
-fn increase1(text: []u8) ?[]u8 {
+fn increase1(text: []u8) bool {
     var index = text.len;
     while (index > 0) {
         index -= 1;
@@ -87,7 +87,7 @@ fn increase1(text: []u8) ?[]u8 {
             continue;
         }
         text[index] += 1;
-        return text;
+        return true;
     }
-    return null;
+    return false;
 }
